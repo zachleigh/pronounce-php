c90c6ea65aa72a4f2ec0da6e4f2a2b74fe915bbc
Zach Leigh <zachleigh@fastmail.jp>
2015-06-21 21:53:40 +0900

    Commit before branching.

--- src/LookupCommand.php
+++ src/LookupCommand.php
@@ -7,6 +7,7 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Console\Helper\Table;
 use PronouncePHP\Transcribe\Transcriber;
 use PronouncePHP\Build\Builder;
